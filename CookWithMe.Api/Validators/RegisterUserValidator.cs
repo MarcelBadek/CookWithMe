e5ebb5644611aa833395c81b69b0e0631aef4c8a
Marcel Badek <kriollik@gmail.com>
2023-07-07 14:32:48 +0200

    feat: add user validator

---
+++ CookWithMe.Api/Validators/RegisterUserValidator.cs
@@ -0,0 +1,16 @@
+﻿using CookWithMe.Data.Entities;
+using CookWithMe.Helpers;
+using FluentValidation;
+
+namespace CookWithMe.Validators;
+
+public class RegisterUserValidator : AbstractValidator<User>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(user => user.UserName).NotEmpty().Length(3, 30).Matches(RegexHelper.LettersAndNumbers);
+        RuleFor(user => user.Email).NotEmpty().EmailAddress();
+        RuleFor(user => user.FirstName).Matches(RegexHelper.Letters);
+        RuleFor(user => user.LastName).Matches(RegexHelper.Letters);
+    }
+}
