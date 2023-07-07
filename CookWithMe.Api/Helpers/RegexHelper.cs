4d840dbf80911ce59885287bb61280fbde9423e7
Marcel Badek <kriollik@gmail.com>
2023-07-07 14:32:35 +0200

    feat: regex helper

---
+++ CookWithMe.Api/Helpers/RegexHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CookWithMe.Helpers;
+
+public static class RegexHelper
+{
+    public static readonly Regex LettersAndNumbers = new Regex("^[a-zA-Z0-9]*$");
+    public static readonly Regex Letters = new Regex("^[a-zA-Z]*$");
+}
