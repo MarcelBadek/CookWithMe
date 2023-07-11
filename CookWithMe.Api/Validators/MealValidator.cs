2b842fb2aa004ba4b606e025b0faafe8bf71072a
Marcel Badek <kriollik@gmail.com>
2023-07-11 14:02:13 +0200

    feat: add meal validator

---
+++ CookWithMe.Api/Validators/MealValidator.cs
@@ -0,0 +1,14 @@
+﻿using CookWithMe.Data.Entities;
+using FluentValidation;
+
+namespace CookWithMe.Validators;
+
+public class MealValidator : AbstractValidator<Meal>
+{
+    public MealValidator()
+    {
+        RuleFor(meal => meal.MealType).NotEmpty().IsInEnum();
+        RuleFor(meal => meal.Name).Length(3, 300);
+        RuleFor(meal => meal.Description).Length(3, 10000);
+    }    
+}
